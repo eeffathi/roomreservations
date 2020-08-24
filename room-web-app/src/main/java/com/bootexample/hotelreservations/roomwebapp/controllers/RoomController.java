c21fd4efe88528cf1df877fdb94f9f6c48139f42
Edris <eeffathi@yahoo.com>
2020-08-24 21:13:31 +0430

    fixing the bug with the controller

--- room-web-app/src/main/java/com/bootexample/hotelreservations/roomwebapp/controllers/RoomController.java
+++ room-web-app/src/main/java/com/bootexample/hotelreservations/roomwebapp/controllers/RoomController.java
@@ -3,6 +3,7 @@ package com.bootexample.hotelreservations.roomwebapp.controllers;
 import com.bootexample.hotelreservations.roomwebapp.entities.Room;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -19,6 +20,7 @@ public class RoomController {
         }
     }
 
+    @GetMapping
     public String getAllRooms(Model model) {
         model.addAttribute("rooms", roomList);
         return "rooms";
